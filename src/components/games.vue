c28d5ae795893cf273d97c6d9f9e01499cbaab6c
ZenzoX <42100836+ZenzoX@users.noreply.github.com>
2020-01-28 15:51:30 +0100

    show game title

---
+++ src/components/games.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-bind:key="game.id" v-for="game in games">
+            <h3>{{game.title}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Games",
+        props: ["games"]
+    }
+</script>
+
+<style scoped>
+
+</style>
